6f1ebc0390a4a51ef866b28955d84438364ab720
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 13:37:06 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,4 +14,9 @@ class CashRegister
       @total += item_total_price
     end
   end
+
+  def apply_discount
+    money_saved = @discount * @total
+    discounted_price = @total - money_saved
+    puts "After the discount, the total comes to $#{discounted_price}."
 end
