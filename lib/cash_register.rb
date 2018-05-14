3693559e8165ca341cc3e3930acf108157d42254
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 13:57:54 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,23 +1,20 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :item_list
 
   def initialize(discount = nil)
     @total = 0
     @discount = discount
-  end
-
-  def item_list
     @item_list = []
   end
-  
+
   def add_item(title, price, quantity = nil)
+    @item_list << title
     if quantity == nil
       @total += price
     else
       item_total_price = price * quantity
       @total += item_total_price
     end
-    @item_list << title
   end
 
   def apply_discount
