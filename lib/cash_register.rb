6e5822cc6530e3630305204b026d6d7da840c705
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 13:40:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,8 +16,12 @@ class CashRegister
   end
 
   def apply_discount
-    money_saved = @discount * @total
-    discounted_price = @total - money_saved
-    puts "After the discount, the total comes to $#{discounted_price}."
+    if @discount == nil
+      "There is no discount to apply."
+    else
+      money_saved = @discount * @total
+      discounted_price = @total - money_saved
+      puts "After the discount, the total comes to $#{discounted_price}."
+    end
   end
 end
