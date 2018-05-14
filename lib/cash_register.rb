d79aac55b58f705ef27bde459c55a224e6c0025c
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-14 14:09:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,8 +18,7 @@ class CashRegister
     else
       item_total_price = price * quantity
       @total += item_total_price
-      quantity.times do |i|
-        @item_list << total
+      quantity.times do @item_list << item
       end
     end
   end
